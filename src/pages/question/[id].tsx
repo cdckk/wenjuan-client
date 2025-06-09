03ce6f98261e43727cc5a785954fc254fb850538
cdckk <1574848757@qq.com>
2025-06-09 22:39:04 +0800

    feat: 添加获取问卷service

--- src/pages/question/[id].tsx
+++ src/pages/question/[id].tsx
@@ -3,13 +3,54 @@ import QuestionRadio from '@/components/QuestionComponents/QuestionRadio'
 import Head from 'next/head'
 import styles from '@/styles/Question.module.scss'
 import PageWrapper from "@/components/PageWrapper";
+import { getQuestionById } from '@/services/question'
 
 type PropsType = {
-  id: string
+  // id: string
+  errno: number,
+  data?: {
+    id: string
+    title: string
+    desc?: string
+    js?: string
+    css?: string
+    isPublished: boolean
+    isDeleted: boolean
+    componentList: Array<any>
+  }
+  msg?: string
 }
 
 export default function Question(props: PropsType) {
-  return <PageWrapper title='question'>
+  const { errno, data, msg = '' } = props
+
+  // 数据错误
+  if (errno !== 0) {
+    return <PageWrapper title='错误'>
+      <h1>错误</h1>
+      <p>{msg}</p>
+    </PageWrapper>
+  }
+
+  const { id, title = '', desc = '', isDeleted, isPublished, componentList = [] } = data || {}
+
+  // 已经被删除，提示错误
+  if (isDeleted) {
+    return <PageWrapper title={title} desc={desc}>
+      <h1>{title}</h1>
+      <p>该问卷已经被删除</p>
+    </PageWrapper>
+  }
+
+  // 尚未发布的，提示错误
+  if (!isPublished) {
+    return <PageWrapper title={title} desc={desc}>
+      <h1>{title}</h1>
+      <p>该问卷尚未发布</p>
+    </PageWrapper>
+  }
+
+  return <PageWrapper title={title} desc={desc}>
     {/* <Head>
       <title>Question</title>
     </Head> */}
@@ -18,7 +59,7 @@ export default function Question(props: PropsType) {
       <p>{props.id}</p> */}
 
       <form method='post' action="/api/answer">
-        <input type='hidden' name='quetionId' defaultValue={props.id} />
+        <input type='hidden' name='quetionId' defaultValue={id} />
         <div className={styles.componentWrapper}>
           <QuestionInput fe_id='c1' props={{ title: '你的姓名', placeholder: '请输入' }}></QuestionInput>
         </div>
@@ -40,4 +81,18 @@ export default function Question(props: PropsType) {
       </form>
     {/* </main> */}
   </PageWrapper>
+}
+
+export async function getServerSideProps(context: any) {
+  const { id = '' } = context.params
+
+  // 根据id 获取问卷数据
+  const data = await getQuestionById(id)
+
+  return {
+    props: {
+      ...data
+      // 其他数据
+    }
+  }
 }
